54193dbd45a348139ee526bb0da24654a8a58436
Tanmay-dobariya <dobariyatanmay@gmail.com>
2025-03-15 16:56:04 +0530

    Fix: #1733 Bugged text message of user and Assistant in search-agent-for-ecommerce got fixed

--- examples/search-agent-for-e-commerce/src/app/actions.tsx
+++ examples/search-agent-for-e-commerce/src/app/actions.tsx
@@ -29,10 +29,13 @@ export async function continueConversation(
 
   const history = getMutableAIState();
 
-  const result = await streamUI({
-    model: openai("gpt-3.5-turbo"),
-    temperature: 0,
-    system: `\
+  const finalResponse = await new Promise<string>((resolve) => {
+    let responseText = "";
+
+    streamUI({
+      model: openai("gpt-3.5-turbo"),
+      temperature: 0,
+      system: `\
     You are a friendly assistant that helps the user with shopping on a ecommerce website ('DUMMY SHOP'). You help users with end-to-end shopping experience
     starting from general information about the brands and products, and helping with product discovery, search, and product details, as well as
     product purchase, customer support, fitting questions, technical questions.
@@ -50,170 +53,182 @@ export async function continueConversation(
     - Ask a clarification/follow up question
     - Product comparison
     - Promotions, hot deals
-    `,
-    messages: [...history.get(), { role: "user", content: input, indexId }],
-    text: ({ content, done }) => {
-      if (done) {
-        history.done((messages: ServerMessage[]) => [
-          ...messages,
-          { role: "assistant", content },
-        ]);
-      }
-      return <div>{content}</div>;
-    },
-    // toolChoice: 'required', // force the model to call a tool
-    // maxToolRoundtrips: 5, // allow up to 5 tool roundtrips
-    tools: {
-      product_search: {
-        description:
-          "Search for products on this website using pre-built indices",
-        parameters: z.object({
-          query: z
-            .string()
-            .describe(
-              "A clear factual product query, potentially including type, name, qualities, characteristics of the product",
-            ),
-        }),
-        generate: async ({ query }) => {
-          try {
-            console.log("query=", query);
-            const response = await fetch(
-              `https://dummyjson.com/products/search?q=${query}`,
-            );
-            const data = (await response.json()) as {
-              products: {
-                thumbnail: string;
-                title: string;
-                description: string;
-                price: string;
-                url: string;
-              }[];
-            };
-            console.log("data=", data);
-            if (data.products && data.products.length > 0) {
-              const products = data.products.map((item) => ({
-                thumbnail: item.thumbnail,
-                title: item.title,
-                description: item.description,
-                metadata_3: item.price,
-                link: item.url,
-              }));
-              return <CarouselPlugin products={products} />;
-            } else {
-              return <p>No products found.</p>;
-            }
-          } catch {
-            return (
-              <p>
-                Sorry, we are experiencing some error. Please refresh the chat
-                and try again.
-              </p>
-            );
-          }
-        },
-      },
-      general_question: {
-        description: "User questions not related to products directly",
-        parameters: z.object({
-          user_question: z
-            .string()
-            .describe("User questions not related to products directly"),
-        }),
-        generate: async function* ({ user_question }) {
-          const filePath = path.resolve(process.cwd(), "public/shop_info.txt");
-          const generalInfo = fs.readFileSync(filePath, "utf-8");
-          const result = streamText({
-            model: openai("gpt-3.5-turbo"),
-            temperature: 0,
-            prompt: `Generate response to user question ${user_question} based on the context ${generalInfo}`,
-          });
-          let textContent = "";
+    ,`,
+      messages: [...history.get(), { role: "user", content: input, indexId }],
+      text: ({ content, done }) => {
+        responseText = content;
 
-          for await (const textPart of result.textStream) {
-            textContent += textPart;
-            yield textContent;
-          }
-          return textContent;
-        },
-      },
-      clothes_fitting: {
-        description:
-          "Send to user link to guidelines for clothes fitting https://images.app.goo.gl/LECaeXJfXa7gzYCC8 ",
-        parameters: z.object({}),
-        generate: async ({}) => {
-          const fittingGuidelinesLink =
-            "https://images.app.goo.gl/LECaeXJfXa7gzYCC8 ";
-          const formattedLink = `<a href="${fittingGuidelinesLink}" target="_blank">Guidelines for clothes fitting</a>`;
-          const linkStyle = {
-            color: "blue",
-            textDecoration: "underline",
-          };
+        if (done) {
           history.done((messages: ServerMessage[]) => [
             ...messages,
-            {
-              role: "assistant",
-              content: formattedLink,
-            },
+            { role: "assistant", content },
           ]);
-
-          return (
-            <a href={fittingGuidelinesLink} target="_blank" style={linkStyle}>
-              Guidelines for clothes fitting
-            </a>
-          );
-        },
+          resolve(responseText);
+        }
+        return content;
       },
-      escalate: {
-        description:
-          "Escalate to human agent if none of the other tools seem relevant or the interaction is repetative, or if the user is getting upset",
-        parameters: z.object({
-          identifiable_info: z
-            .string()
-            .describe(
-              "Email, full name, or order number to make the request identifiable",
-            ),
-          summary: z.string().describe("Summarize user request concisely"),
-        }),
-        generate: async function* ({ identifiable_info, summary }) {
-          let textContent =
-            "I am escalating your question to the human assistant\n\n";
-          yield textContent;
-          console.log(
-            "generating answer while escalating, ",
-            identifiable_info,
-          );
-          const filePath = path.resolve(process.cwd(), "public/shop_info.txt");
-          const generalInfo = fs.readFileSync(filePath, "utf-8");
-          const result = await streamText({
-            model: openai("gpt-3.5-turbo"),
-            temperature: 0,
-            system: `Generate response to user question ${summary} based on the context ${generalInfo}. Your answer begins with: "While we wait for the human assistant,`,
-            messages: [
-              {
-                role: "user",
-                content: summary,
-              },
+      tools: {
+        product_search: {
+          description:
+            "Search for products on this website using pre-built indices",
+          parameters: z.object({
+            query: z
+              .string()
+              .describe(
+                "A clear factual product query, potentially including type, name, qualities, characteristics of the product",
+              ),
+          }),
+          generate: async ({ query }) => {
+            try {
+              console.log("query=", query);
+              const response = await fetch(
+                `https://dummyjson.com/products/search?q=${query}`,
+              );
+              const data = (await response.json()) as {
+                products: {
+                  thumbnail: string;
+                  title: string;
+                  description: string;
+                  price: string;
+                  url: string;
+                }[];
+              };
+              if (data.products && data.products.length > 0) {
+                const products = data.products.map((item) => ({
+                  thumbnail: item.thumbnail,
+                  title: item.title,
+                  description: item.description,
+                  metadata_3: item.price,
+                  link: item.url,
+                }));
+                return <CarouselPlugin products={products} />;
+              } else {
+                return <p>No products found.</p>;
+              }
+            } catch {
+              return (
+                <p>
+                  Sorry, we are experiencing some error. Please refresh the chat
+                  and try again.
+                </p>
+              );
+            }
+          },
+        },
+        general_question: {
+          description: "User questions not related to products directly",
+          parameters: z.object({
+            user_question: z
+              .string()
+              .describe("User questions not related to products directly"),
+          }),
+          generate: async function* ({ user_question }) {
+            const filePath = path.resolve(
+              process.cwd(),
+              "public/shop_info.txt",
+            );
+            const generalInfo = fs.readFileSync(filePath, "utf-8");
+            const result = streamText({
+              model: openai("gpt-3.5-turbo"),
+              temperature: 0,
+              prompt: `Generate response to user question ${user_question} based on the context ${generalInfo}`,
+            });
+            let textContent = "";
+
+            for await (const textPart of result.textStream) {
+              textContent += textPart;
+              yield textContent;
+            }
+            return textContent;
+          },
+        },
+        clothes_fitting: {
+          description:
+            "Send to user link to guidelines for clothes fitting https://images.app.goo.gl/LECaeXJfXa7gzYCC8 ",
+          parameters: z.object({}),
+          generate: async ({}) => {
+            const fittingGuidelinesLink =
+              "https://images.app.goo.gl/LECaeXJfXa7gzYCC8 ";
+            const formattedLink = `<a href="${fittingGuidelinesLink}" target="_blank">Guidelines for clothes fitting</a>`;
+            const linkStyle = {
+              color: "blue",
+              textDecoration: "underline",
+            };
+            history.done((messages: ServerMessage[]) => [
+              ...messages,
               {
                 role: "assistant",
-                content: textContent,
+                content: formattedLink,
               },
-            ],
-          });
+            ]);
 
-          for await (const textPart of result.textStream) {
-            textContent += textPart;
+            return (
+              <a href={fittingGuidelinesLink} target="_blank" style={linkStyle}>
+                Guidelines for clothes fitting
+              </a>
+            );
+          },
+        },
+        escalate: {
+          description:
+            "Escalate to human agent if none of the other tools seem relevant or the interaction is repetative, or if the user is getting upset",
+          parameters: z.object({
+            identifiable_info: z
+              .string()
+              .describe(
+                "Email, full name, or order number to make the request identifiable",
+              ),
+            summary: z.string().describe("Summarize user request concisely"),
+          }),
+          generate: async function* ({ identifiable_info, summary }) {
+            let textContent =
+              "I am escalating your question to the human assistant\n\n";
             yield textContent;
-          }
-          return textContent;
+            console.log(
+              "Generating answer while escalating...",
+              identifiable_info !== "N/A"
+                ? identifiable_info
+                : "No identifiable info provided",
+            );
+            const filePath = path.resolve(
+              process.cwd(),
+              "public/shop_info.txt",
+            );
+            const generalInfo = fs.readFileSync(filePath, "utf-8");
+            const result = await streamText({
+              model: openai("gpt-3.5-turbo"),
+              temperature: 0,
+              system: `Generate response to user question ${summary} based on the context ${generalInfo}. Your answer begins with: "While we wait for the human assistant,`,
+              messages: [
+                {
+                  role: "user",
+                  content: summary,
+                },
+                {
+                  role: "assistant",
+                  content: textContent,
+                },
+              ],
+            });
+
+            for await (const textPart of result.textStream) {
+              textContent += textPart;
+              yield textContent;
+            }
+            return textContent;
+          },
         },
       },
-    },
+    });
   });
 
   return {
     id: nanoid(),
     role: "assistant",
-    display: result.value,
+    display:
+      finalResponse.length > 0
+        ? finalResponse
+        : "Failed to generate a response",
   };
 }
 
