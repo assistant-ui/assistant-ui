5d8f08540fa41472bd1f34f307aafacf4c0039a0
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2025-04-14 01:51:39 -0400

    improve syntax highlighter with styling and prop support
    - Add `className` prop support
    - Implement styling for code blocks (overflow handling and padding)
    - Merge default and passed class names with cn
    - Disable default react-shiki styles for more control
    - Omit `addDefaultStyles` in ShikiConfig, this is permanently disabled in assistant-ui/react-shiki
      - Figure out why this Omit doesn't remove autocomplete in `makeShikiHighlighter`

--- packages/react-shiki/src/react-shiki.tsx
+++ packages/react-shiki/src/react-shiki.tsx
@@ -3,18 +3,37 @@
 import { ComponentType, type FC } from "react";
 import ShikiHighlighter, { type ShikiHighlighterProps } from "react-shiki";
 import type { SyntaxHighlighterProps } from "@assistant-ui/react-markdown";
+import { cn } from "../../../apps/registry/lib/utils";
 
-type ShikiConfig = Omit<ShikiHighlighterProps, "language" | "children">;
+type ExtendedSyntaxHighlighterProps = SyntaxHighlighterProps & {
+  className?: string;
+};
+
+// TODO: Figure out why addDefaultStyles is still showing up in autocomplete for ShikiOptions. Maybe types didn't update after omitting it?
+type ShikiConfig = Omit<
+  ShikiHighlighterProps,
+  "language" | "children" | "addDefaultStyles"
+>;
 
 const makeMakeShikiHighlighter =
   (BaseHighlighter: ComponentType<ShikiHighlighterProps>) =>
   (config: ShikiConfig) => {
-    const ShikiSyntaxHighlighter: FC<SyntaxHighlighterProps> = ({
+    const ShikiSyntaxHighlighter: FC<ExtendedSyntaxHighlighterProps> = ({
       language,
       code,
+      className,
     }) => {
       return (
-        <BaseHighlighter {...config} language={language}>
+        <BaseHighlighter
+          {...config}
+          language={language}
+          className={cn(
+            "[&_pre]:overflow-x-auto [&_pre]:px-4 [&_pre]:py-2",
+            config.className,
+            className,
+          )}
+          addDefaultStyles={false}
+        >
           {code}
         </BaseHighlighter>
       );
