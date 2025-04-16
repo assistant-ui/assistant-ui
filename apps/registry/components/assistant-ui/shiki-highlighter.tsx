eaaf8ec8c7c313bb167dae0ca9cddbab1b9203d9
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2025-04-16 01:00:34 -0400

    first working version of registry component implementation

--- apps/registry/components/assistant-ui/shiki-highlighter.tsx
+++ apps/registry/components/assistant-ui/shiki-highlighter.tsx
@@ -1,30 +1,42 @@
 "use client";
 
-import { FC } from "react";
-import BaseShikiHighlighter, {
-  type ShikiHighlighterProps as BaseShikiProps,
-} from "react-shiki";
+import { lazy, Suspense, FC } from "react";
+// import type { SyntaxHighlighterProps } from "@assistant-ui/react-markdown";
+import type { ShikiHighlighterProps as BaseShikiProps } from "react-shiki";
 
-// omit props used for internal config
-type ShikiHighlighterProps = Omit<BaseShikiProps, "addDefaultStyles" | "showLanguage">;
+// create lazy loaded component that solves issue with component initialization
+// previously it was initializing before react-shiki was loaded, causing errors
+// the factory pattern created a component function that was evaluated later, ensuring react-shiki was loaded
+const ShikiModule = lazy<React.ComponentType<BaseShikiProps>>(
+  () => import("react-shiki"),
+);
 
-export const ShikiHighlighter: FC<ShikiHighlighterProps> = ({
-  children: code,
-  ...props
+type ShikiSyntaxHighlighterProps = Omit<
+  BaseShikiProps,
+  "addDefaultStyles" | "showLanguage" | "children" | "theme"
+> & { code: string };
+
+export const SyntaxHighlighter: FC<ShikiSyntaxHighlighterProps> = ({
+  language,
+  code,
 }) => {
   return (
-    <BaseShikiHighlighter
-      addDefaultStyles={false}
-      showLanguage={false}
-      style={{
-        margin: 0,
-        width: "100%",
-        background: "black",
-        padding: "1.5rem 1rem",
-      }}
-      {...props}
+    <Suspense
+      fallback={
+        <pre>
+          <code>{code}</code>
+        </pre>
+      }
     >
-      {code}
-    </BaseShikiHighlighter>
+      <ShikiModule
+        language={language}
+        theme={"andromeeda"}
+        className="[&_pre]:overflow-x-auto [&_pre]:px-4 [&_pre]:py-2"
+        addDefaultStyles={false}
+        showLanguage={false}
+      >
+        {code}
+      </ShikiModule>
+    </Suspense>
   );
 };
