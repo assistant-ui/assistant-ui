cb0069c7a7d720f0a322b0a373d76b55286ae7e6
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2025-04-16 16:00:14 -0400

    working component using hoc

--- apps/registry/components/assistant-ui/shiki-highlighter.tsx
+++ apps/registry/components/assistant-ui/shiki-highlighter.tsx
@@ -1,42 +1,32 @@
 "use client";
 
-import { lazy, Suspense, FC } from "react";
+import { FC } from "react";
+import ShikiHighlighter, { type ShikiHighlighterProps as BaseShikiProps } from "react-shiki";
 // import type { SyntaxHighlighterProps } from "@assistant-ui/react-markdown";
-import type { ShikiHighlighterProps as BaseShikiProps } from "react-shiki";
-
-// create lazy loaded component that solves issue with component initialization
-// previously it was initializing before react-shiki was loaded, causing errors
-// the factory pattern created a component function that was evaluated later, ensuring react-shiki was loaded
-const ShikiModule = lazy<React.ComponentType<BaseShikiProps>>(
-  () => import("react-shiki"),
-);
 
 type ShikiSyntaxHighlighterProps = Omit<
   BaseShikiProps,
   "addDefaultStyles" | "showLanguage" | "children" | "theme"
 > & { code: string };
 
-export const SyntaxHighlighter: FC<ShikiSyntaxHighlighterProps> = ({
+// Higher order component solves initialization issue
+export const SyntaxHighlighter: FC<ShikiSyntaxHighlighterProps> = (props) => {
+  return <SH {...props} />;
+};
+
+const SH: FC<ShikiSyntaxHighlighterProps> = ({
   language,
   code,
 }) => {
   return (
-    <Suspense
-      fallback={
-        <pre>
-          <code>{code}</code>
-        </pre>
-      }
+    <ShikiHighlighter
+      language={language}
+      theme="andromeeda"
+      className="[&_pre]:px-4 [&_pre]:py-2 [&_pre]:overflow-x-auto [&_pre]:rounded-b-lg [&_pre]:bg-black [&_pre]:p-4 [&_pre]:text-white"
+      addDefaultStyles={false}
+      showLanguage={false}
     >
-      <ShikiModule
-        language={language}
-        theme={"andromeeda"}
-        className="[&_pre]:overflow-x-auto [&_pre]:px-4 [&_pre]:py-2"
-        addDefaultStyles={false}
-        showLanguage={false}
-      >
-        {code}
-      </ShikiModule>
-    </Suspense>
+      {code}
+    </ShikiHighlighter>
   );
 };
