152ffa6b2b5dcff6f18ccbceea180d810e8c29c3
Christian Glassiognon <63924603+heyglassy@users.noreply.github.com>
2025-05-25 11:42:14 -0700

    updated with toolbox types somewhat working

--- examples/with-cloud/app/MyRuntimeProvider.tsx
+++ examples/with-cloud/app/MyRuntimeProvider.tsx
@@ -6,17 +6,67 @@ import { useChatRuntime } from "@assistant-ui/react-ai-sdk";
 // CG TODO: cleanup this import
 import { createToolbox } from "@assistant-ui/react/model-context/tool";
 import type { BackendTools } from "./api/chat/route";
+import { z } from "zod";
 
 const cloud = new AssistantCloud({
   baseUrl: process.env["NEXT_PUBLIC_ASSISTANT_BASE_URL"]!,
   anonymous: true,
 });
 
-const toolbox = createToolbox<BackendTools>({
-  weather: true,
-  day: true,
-  rain: true,
-});
+// const getUsersCity = {}; // define w frontend tool
+
+// const toolbox = createToolbox<BackendTools>({
+//   weather: true,
+//   day: true,
+//   rain: true,
+//   // getCountry: {
+//   //   type: "frontend",
+//   //   parameters: {
+//   //     type: "object",
+//   //     properties: {
+//   //       name: { type: "string" },
+//   //     },
+//   //   },
+//   // }, // HITL
+//   // userTimezone: {
+//   //   type: "frontend",
+//   //   parameters: z.undefined(),
+//   //   execute: async () => {
+//   //     return "Europe/Berlin";
+//   //   },
+//   //   render: (args) => <div>User timezone: {args}</div>,
+//   //   // parameters: z.object({
+//   //   //   timezone: z.string(),
+//   //   // }),
+//   // },
+// });
+
+// const toolbox2 = createToolbox<BackendTools>();
+// .tool("weather", {
+//   render: (args) => <div>Weather: {args}</div>,
+// })
+// .tool("other", {
+//   type: "frontend",
+//   parameters: z.object({
+//     name: z.string(),
+//   }),
+//   execute: async (args) => {
+//     return "Other";
+//   },
+// });
+
+const toolbox = createToolbox<BackendTools>()
+  .tool("test", {
+    parameters: z.object({
+      name: z.string(),
+    }),
+    execute: async (args) => {
+      return `Other: ${args.name}`;
+    },
+  })
+  .tool("weather", {
+    render: (args) => <div>Weather: {args.weather}</div>,
+  });
 
 console.log(toolbox);
 
