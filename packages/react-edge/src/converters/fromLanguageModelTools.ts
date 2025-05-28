6f24ba9912d70ffd0aa37814243a83050f9f87da
Christian Glassiognon <63924603+heyglassy@users.noreply.github.com>
2025-05-28 00:48:20 -0700

    nit 5

--- packages/react-edge/src/converters/fromLanguageModelTools.ts
+++ packages/react-edge/src/converters/fromLanguageModelTools.ts
@@ -5,15 +5,7 @@ import { Tool } from "@assistant-ui/react";
  * @deprecated This is an internal API and may change without notice.
  */
 export const fromLanguageModelTools = (
-  tools: LanguageModelV1FunctionTool[],
-): Record<string, Tool<any, any>> => {
-  return Object.fromEntries(
-    tools.map((tool) => [
-      tool.name,
-      {
-        ...tool,
-        type: tool.type as unknown as "human" | "frontend" | "backend",
-      },
-    ]),
-  );
+  tools: LanguageModelV1FunctionTool[] | Tool<any, any>[],
+): Record<string, Omit<Tool<any, any>, "type">> => {
+  return Object.fromEntries(tools.map((tool) => [tool.name, tool]));
 };
