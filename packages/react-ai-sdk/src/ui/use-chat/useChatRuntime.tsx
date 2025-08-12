6ec370fec10dd3f06a2ec6d218e3a2a761d81a06
Simon Farshid <simon.farshid@outlook.com>
2025-08-11 19:20:45 -0700

    feat: AssistantChatTransport

--- packages/react-ai-sdk/src/ui/use-chat/useChatRuntime.tsx
+++ packages/react-ai-sdk/src/ui/use-chat/useChatRuntime.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-import { z } from "zod";
 import { useChat, type UIMessage } from "@ai-sdk/react";
 import type { AssistantCloud } from "assistant-cloud";
 import {
   AssistantRuntime,
-  Tool,
   unstable_useCloudThreadListAdapter,
   unstable_useRemoteThreadListRuntime,
 } from "@assistant-ui/react";
 import { useAISDKRuntime, type AISDKRuntimeAdapter } from "./useAISDKRuntime";
-import { ChatInit, DefaultChatTransport, JSONSchema7 } from "ai";
+import { ChatInit } from "ai";
+import { AssistantChatTransport } from "./AssistantChatTransport";
 
 export type UseChatRuntimeOptions<UI_MESSAGE extends UIMessage = UIMessage> =
   ChatInit<UI_MESSAGE> & {
@@ -18,50 +17,26 @@ export type UseChatRuntimeOptions<UI_MESSAGE extends UIMessage = UIMessage> =
     adapters?: AISDKRuntimeAdapter["adapters"] | undefined;
   };
 
-const toAISDKTools = (tools: Record<string, Tool>) => {
-  return Object.fromEntries(
-    Object.entries(tools).map(([name, tool]) => [
-      name,
-      {
-        ...(tool.description ? { description: tool.description } : undefined),
-        parameters: (tool.parameters instanceof z.ZodType
-          ? z.toJSONSchema(tool.parameters)
-          : tool.parameters) as JSONSchema7,
-      },
-    ]),
-  );
-};
-
-const getEnabledTools = (tools: Record<string, Tool>) => {
-  return Object.fromEntries(
-    Object.entries(tools).filter(
-      ([, tool]) => !tool.disabled && tool.type !== "backend",
-    ),
-  );
-};
-
 export const useChatThreadRuntime = <UI_MESSAGE extends UIMessage = UIMessage>(
   options?: UseChatRuntimeOptions<UI_MESSAGE>,
 ): AssistantRuntime => {
-  const { adapters, ...chatOptions } = options ?? {};
+  const {
+    adapters,
+    transport: transportOptions,
+    ...chatOptions
+  } = options ?? {};
+  const transport = transportOptions ?? new AssistantChatTransport();
+
   const chat = useChat({
     ...chatOptions,
-    transport:
-      chatOptions.transport ??
-      new DefaultChatTransport({
-        prepareSendMessagesRequest(opt) {
-          const context = runtime.thread.getModelContext();
-          return {
-            body: {
-              system: context?.system,
-              tools: toAISDKTools(getEnabledTools(context.tools ?? {})),
-              ...opt.body,
-            },
-          };
-        },
-      }),
+    transport,
   });
+
   const runtime = useAISDKRuntime(chat as any, { adapters });
+  if (transport instanceof AssistantChatTransport) {
+    transport.setRuntime(runtime);
+  }
+
   return runtime;
 };
 
