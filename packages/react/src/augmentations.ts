0410ed12306cadee895bde12723459de23db58cf
GitHub Action <action@github.com>
2025-09-28 16:07:43 +0000

    style: fix prettier issues

--- packages/react/src/augmentations.ts
+++ packages/react/src/augmentations.ts
@@ -6,15 +6,16 @@ declare global {
 
 type GetAugmentation<
   Key extends keyof Assistant,
-  ExpectedType
+  ExpectedType,
 > = unknown extends Assistant[Key]
   ? ExpectedType
   : Assistant[Key] extends ExpectedType
-  ? Assistant[Key]
-  : {
-      ErrorMessage: `There is an error in the type you provided for Assistant.${Key}`;
-    };
+    ? Assistant[Key]
+    : {
+        ErrorMessage: `There is an error in the type you provided for Assistant.${Key}`;
+      };
 
 type UserCommandsRecord = GetAugmentation<"Commands", Record<string, unknown>>;
 
-export type UserCommands = UserCommandsRecord extends Record<string, infer V> ? V : never;
+export type UserCommands =
+  UserCommandsRecord extends Record<string, infer V> ? V : never;
