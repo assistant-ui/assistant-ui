ff25cd2c7d92d1f1f7c9a46eaab3042e7fc37be0
Simon Farshid <simon.farshid@outlook.com>
2025-09-28 15:23:41 -0700

    fix eslint warnings

--- apps/docs/components/ui/TextShimmer.tsx
+++ apps/docs/components/ui/TextShimmer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useMemo, type JSX, type CSSProperties } from "react";
+import React, { useMemo, type JSX } from "react";
 import { motion } from "motion/react";
 import { cn } from "@/lib/utils";
 
