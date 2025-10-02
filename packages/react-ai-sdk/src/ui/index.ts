173e88af3a2d2202b359740a09a6cfeca0bb6487
samdickson22 <sdicks02@calpoly.edu>
2025-10-01 17:14:35 -0700

    exporting UseChatRuntimeOptions and useChatThreadRuntime

--- packages/react-ai-sdk/src/ui/index.ts
+++ packages/react-ai-sdk/src/ui/index.ts
@@ -1,3 +1,4 @@
 export { useAISDKRuntime } from "./use-chat/useAISDKRuntime";
-export { useChatRuntime } from "./use-chat/useChatRuntime";
+export { useChatRuntime, useChatThreadRuntime } from "./use-chat/useChatRuntime";
+export type { UseChatRuntimeOptions } from "./use-chat/useChatRuntime";
 export { AssistantChatTransport } from "./use-chat/AssistantChatTransport";
