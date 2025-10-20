2b1c32970a3fa7de2750ee8e67e520d3eea1c62e
jasontoups <me@jasontoups.com>
2025-10-20 14:19:00 -0700

    updates examples layout

--- apps/docs/app/(home)/examples/layout.config.tsx
+++
@@ -1,9 +0,0 @@
-import { DocsLayoutProps } from "fumadocs-ui/layouts/docs";
-import { sharedDocsOptions } from "@/app/docs/layout.config";
-import { examplesPageTree } from "@/app/source";
-
-// examples layout configuration
-export const examplesOptions: DocsLayoutProps = {
-  ...sharedDocsOptions,
-  tree: examplesPageTree,
-};
--- apps/docs/app/(home)/examples/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { DocsLayout } from "fumadocs-ui/layouts/docs";
-import type { ReactNode } from "react";
-import { examplesOptions } from "./layout.config";
-
-export default function Layout({ children }: { children: ReactNode }) {
-  return <DocsLayout {...examplesOptions}>{children}</DocsLayout>;
-}
