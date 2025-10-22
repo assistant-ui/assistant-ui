33f1aa4057ba84ba70092626dca6724b444c0351
jasontoups <me@jasontoups.com>
2025-10-22 08:55:14 -0700

    useCloudRuntime - removes comment about production readyness

--- packages/react-data-stream/src/useCloudRuntime.ts
+++ packages/react-data-stream/src/useCloudRuntime.ts
@@ -9,9 +9,6 @@ type UseCloudRuntimeOptions = Omit<UseDataStreamRuntimeOptions, "api"> & {
   assistantId: string;
 };
 
-/**
- * @deprecated This is under active development and not yet ready for prod use.
- */
 export const useCloudRuntime = (options: UseCloudRuntimeOptions) => {
   const opts = options.cloud.runs.__internal_getAssistantOptions(
     options.assistantId,
