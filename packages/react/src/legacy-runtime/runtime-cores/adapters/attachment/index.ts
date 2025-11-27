d80406b0be5d8edfdb6f1c4d6516b50397f6e40b
samdickson22 <sdicks02@calpoly.edu>
2025-11-26 16:16:48 -0800

    Merge upstream/main into native_audio_video

--- packages/react/src/legacy-runtime/runtime-cores/adapters/attachment/index.ts
+++ packages/react/src/legacy-runtime/runtime-cores/adapters/attachment/index.ts
@@ -1,5 +1,7 @@
 export type { AttachmentAdapter } from "./AttachmentAdapter";
 export { SimpleImageAttachmentAdapter } from "./SimpleImageAttachmentAdapter";
+export { SimpleAudioAttachmentAdapter } from "./SimpleAudioAttachmentAdapter";
+export { SimpleVideoAttachmentAdapter } from "./SimpleVideoAttachmentAdapter";
 export { SimpleTextAttachmentAdapter } from "./SimpleTextAttachmentAdapter";
 export { CompositeAttachmentAdapter } from "./CompositeAttachmentAdapter";
 export { CloudFileAttachmentAdapter } from "./CloudFileAttachmentAdapter";
