fddb714bb4b1be1a0bdd0702ac320c42ebc5bd13
Harry Yep <git@hode.co.uk>
2026-02-16 21:09:06 +0800

    feat(core): types

---
+++ packages/core/src/types/index.ts
@@ -0,0 +1,36 @@
+export type {
+  Attachment,
+  PendingAttachment,
+  CompleteAttachment,
+  AttachmentStatus,
+} from "./AttachmentTypes";
+
+export type {
+  AppendMessage,
+  TextMessagePart,
+  ReasoningMessagePart,
+  SourceMessagePart,
+  ImageMessagePart,
+  FileMessagePart,
+  DataMessagePart,
+  Unstable_AudioMessagePart,
+  ToolCallMessagePart,
+  MessageStatus,
+  MessagePartStatus,
+  ToolCallMessagePartStatus,
+  MessageTiming,
+  // thread message types
+  ThreadUserMessagePart,
+  ThreadAssistantMessagePart,
+  ThreadSystemMessage,
+  ThreadAssistantMessage,
+  ThreadUserMessage,
+  ThreadMessage,
+  RunConfig,
+  MessageRole,
+  ThreadStep,
+} from "./AssistantTypes";
+
+export type { Unsubscribe } from "./Unsubscribe";
+
+export type { QuoteInfo } from "./QuoteTypes";
