c3bfb1988739051dc6c52c7d9cb2aa6a125e838b
Harry Yep <git@hode.co.uk>
2026-02-18 20:24:48 +0800

    feat(react-langgraph): messages tuple

--- examples/with-langgraph/app/MyRuntimeProvider.tsx
+++ examples/with-langgraph/app/MyRuntimeProvider.tsx
@@ -2,28 +2,37 @@
 
 import { AssistantRuntimeProvider } from "@assistant-ui/react";
 import { useLangGraphRuntime } from "@assistant-ui/react-langgraph";
-import { createThread, sendMessage } from "@/lib/chatApi";
-import { useRef } from "react";
+import { createThread, getThreadState, sendMessage } from "@/lib/chatApi";
+import { LangChainMessage } from "@assistant-ui/react-langgraph";
 
 export function MyRuntimeProvider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const threadIdRef = useRef<string | undefined>(undefined);
   const runtime = useLangGraphRuntime({
-    stream: async function* (messages) {
-      if (!threadIdRef.current) {
-        const { thread_id } = await createThread();
-        threadIdRef.current = thread_id;
-      }
+    stream: async function* (messages, { initialize }) {
+      const { externalId } = await initialize();
+      if (!externalId) throw new Error("Thread not found");
 
       const generator = sendMessage({
-        threadId: threadIdRef.current,
+        threadId: externalId,
         messages,
       });
 
       yield* generator;
+    },
+    create: async () => {
+      const { thread_id } = await createThread();
+      return { externalId: thread_id };
+    },
+    load: async (externalId) => {
+      const state = await getThreadState(externalId);
+      return {
+        messages:
+          (state.values as { messages?: LangChainMessage[] }).messages ?? [],
+        interrupts: state.tasks[0]?.interrupts ?? [],
+      };
     },
     eventHandlers: {
       onMessageChunk: (chunk, metadata) => {
