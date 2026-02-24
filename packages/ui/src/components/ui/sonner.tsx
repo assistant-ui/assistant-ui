292fb0d708dab8bef7f9d2130878453372d7db5d
Harry Yep <git@hode.co.uk>
2026-02-24 10:28:39 +0800

    feat(ui): use latest shadcn/ui

--- packages/ui/src/components/ui/sonner.tsx
+++ packages/ui/src/components/ui/sonner.tsx
@@ -10,14 +10,12 @@ import {
 import { useTheme } from "next-themes";
 import { Toaster as Sonner, type ToasterProps } from "sonner";
 
-const Toaster = ({ theme: themeProp, ...props }: ToasterProps) => {
-  const { theme: themeContext } = useTheme();
-  const theme =
-    themeProp ?? (themeContext as "light" | "dark" | "system") ?? "system";
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme();
 
   return (
     <Sonner
-      theme={theme}
+      theme={theme as ToasterProps["theme"]}
       className="toaster group"
       icons={{
         success: <CircleCheckIcon className="size-4" />,
