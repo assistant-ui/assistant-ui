75939db9c7481901a3901a8223c14a134ce348eb
Simon Farshid <simon.farshid@outlook.com>
2026-04-13 13:19:10 -0700

    fix(tap): declare process type locally to avoid @types/node dependency

--- packages/tap/src/core/helpers/env.ts
+++ packages/tap/src/core/helpers/env.ts
@@ -1,3 +1,5 @@
+declare const process: undefined | { env?: Record<string, string | undefined> };
+
 export const isDevelopment =
   typeof process !== "undefined" &&
-  (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test");
+  (process.env?.NODE_ENV === "development" || process.env?.NODE_ENV === "test");
