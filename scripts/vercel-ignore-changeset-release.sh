5cb0c38b45dab4e482c44f65a1ec39d90f73b4e6
Simon Farshid <simon.farshid@outlook.com>
2026-06-01 15:50:00 -0700

    chore: narrow Vercel version branch skip

--- scripts/vercel-ignore-changeset-release.sh
+++ scripts/vercel-ignore-changeset-release.sh
@@ -5,7 +5,7 @@ commit_ref="${VERCEL_GIT_COMMIT_REF:-}"
 commit_message="${VERCEL_GIT_COMMIT_MESSAGE:-}"
 
 case "$commit_ref" in
-  changeset-release/* | chore/update-versions | chore-update-versions | chore-update-versions-*)
+  changeset-release/*)
     echo "Skipping Vercel build for Changesets version branch: $commit_ref"
     exit 0
     ;;
