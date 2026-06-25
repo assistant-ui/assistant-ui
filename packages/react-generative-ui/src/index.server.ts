9e9b194318ac2e4353dde0d7273798f8beee15ba
Simon Farshid <simon.farshid@outlook.com>
2026-06-24 22:39:19 -0700

    fix: share generative ui root exports

--- packages/react-generative-ui/src/index.server.ts
+++ packages/react-generative-ui/src/index.server.ts
@@ -1,21 +1,2 @@
 export { JSONGenerativeUI } from "./JSONGenerativeUI.server";
-export { renderGenerativeUI } from "./renderGenerativeUI";
-export { generativeUIToJSX } from "./generativeUIToJSX";
-export { buildPresentParameters } from "./buildPresentParameters";
-export { defineGenerativeComponents } from "./defineGenerativeComponents";
-export { TYPE_KEY } from "./constants";
-export type {
-  JSONGenerativeUIOptions,
-  PresentTool,
-  PresentToolOptions,
-  PromptUserTool,
-} from "./JSONGenerativeUI.shared";
-export type {
-  GenerativeUILibrary,
-  GenerativeUIComponent,
-  GenerativeUIElement,
-  GenerativeUIProps,
-  GenerativeUINode,
-  GenerativeUIStatus,
-  GenerativeUIRenderContext,
-} from "./types";
+export * from "./index.shared";
