04b9f4845b74ffff51b49a327f04c5b672389f94
Harry Yep <git@hode.co.uk>
2026-09-20 18:52:04 +0800

    feat(react-native): export ToolCallMessagePartStatus

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -22,6 +22,7 @@ export type {
   ToolApprovalOptionKind,
   ToolApprovalResponse,
   ToolCallMessagePart,
+  ToolCallMessagePartStatus,
   ToolCallTiming,
   ToolModelContentPart,
   ImageMessagePart,
